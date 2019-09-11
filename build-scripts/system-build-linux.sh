b5bc2b0d70a7a977d87559f50d987f92fbe18f16
Gijs Peskens <gijs@in2ip.nl>
2019-09-11 12:27:15 +0000

    modify for easier packaging

--- build-scripts/system-build-linux.sh
+++ build-scripts/system-build-linux.sh
@@ -6,7 +6,7 @@ fail()
     exit 1
 }
 
-BUILD_ARCHIVE_NAME="server"
+BUILD_ARCHIVE_NAME="../OUT/opt/casparcgserver/"
 
 # Clean and enter shadow build folder
 echo Cleaning...
@@ -46,16 +46,16 @@ LIB_DIR="$SERVER_FOLDER/lib"
 DOC_DIR="$SERVER_FOLDER/doc/"
 FONT_DIR="$SERVER_FOLDER/fonts"
 
-mkdir "$SERVER_FOLDER" || fail "Could not create $SERVER_FOLDER"
+mkdir -p "$SERVER_FOLDER" || fail "Could not create $SERVER_FOLDER"
 mkdir -p "$BIN_DIR"    || fail "Could not create $BIN_DIR"
 mkdir -p "$LIB_DIR"    || fail "Could not create $LIB_DIR"
-mkdir -p "$DOC_DIR"    || fail "Could not create $DOC_DIR"
 mkdir -p "$FONT_DIR"   || fail "Could not create $FONT_DIR"
 
 # Copy compiled binaries
 echo Copying binaries...
 cp -fa  shell/lib* "$LIB_DIR/" 2>/dev/null         || echo "Did not copy server libraries"
 cp -fa  shell/*.ttf "$FONT_DIR/" 2>/dev/null       || echo "Did not copy fonts"
+cp -fa  ../deploy/general/server/font/LiberationSans-Regular.ttf "$SERVER_FOLDER" || echo "Could not copy liberation font"
 cp -fa  shell/casparcg "$BIN_DIR/casparcg-server"  || fail "Could not copy server executable"
 cp -fa  shell/casparcg.config "$DOC_DIR/"          || fail "Could not copy server config"
 cp -faR shell/locales "$BIN_DIR/" 2>/dev/null      || echo "Did not copy server CEF locales"
@@ -64,24 +64,8 @@ cp -f  shell/*.pak "$SERVER_FOLDER/bin/" || fail "Could not copy CEF resources"
 cp -f  shell/*.bin "$SERVER_FOLDER/bin/" || fail "Could not copy V8 resources"
 cp -f  shell/*.dat "$SERVER_FOLDER/bin/" || fail "Could not copy ICU resources"
 
-
-# Copy binary dependencies
-#echo Copying binary dependencies...
-#cp -fa  ../deploy/general/*.pdf "$DOC_DIR/"        || fail "Could not copy pdf"
-#cp -faR ../deploy/general/wallpapers "$DOC_DIR/"   || fail "Could not copy wallpapers"
-#cp -faR ../deploy/general/server/media "$DOC_DIR/" || fail "Could not copy media"
-
-# Copy documentation
-#echo Copying documentation...
-#cp -fa  ../CHANGELOG "$DOC_DIR" || fail "Could not copy CHANGES.txt"
-#cp -fa  ../README    "$DOC_DIR" || fail "Could not copy README.txt"
-#cp -fa  ../LICENSE   "$DOC_DIR" || fail "Could not copy LICENSE.txt"
-
 # Remove empty directories
 rmdir "$LIB_DIR" 2>/dev/null
 rmdir "$FONT_DIR" 2>/dev/null
 
-# Create tar.gz file
-echo Creating tag.gz...
-tar -cvzf "$BUILD_ARCHIVE_NAME.tar.gz" "$SERVER_FOLDER" || fail "Could not create archive"
 
